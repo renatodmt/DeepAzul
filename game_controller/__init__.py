83857d2ab10aa120d52f2371bbdd105d35eeb832
renatodmt <renatodmt@gmail.com>
2024-04-01 12:12:54 -0300

    Change init

--- game_controller/__init__.py
+++ game_controller/__init__.py
@@ -1,2 +1,2 @@
-from game_controller.azul_game_controller import AzulGameController
-from game_controller.title_colors import TITLE_COLORS
+from DeepAzul.game_controller.azul_game_controller import AzulGameController
+from DeepAzul.game_controller.title_colors import TITLE_COLORS
