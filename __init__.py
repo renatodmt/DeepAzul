0052b9eeea20c4d36d9fe7c1a3f44ce24d2ae41c
renatodmt <renatodmt@gmail.com>
2024-04-01 11:53:41 -0300

    Removed init

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from DeepAzul.game_controller import AzulGameController
-from DeepAzul.data_processing import DataProcessor
-from DeepAzul.models import SimpleNeuralNetwork
-from DeepAzul.agents import RandomAgent, NeuralNetworkAgent
+#from DeepAzul.game_controller import AzulGameController
+#from DeepAzul.data_processing import DataProcessor
+#from DeepAzul.models import SimpleNeuralNetwork
+#from DeepAzul.agents import RandomAgent, NeuralNetworkAgent
